ca763618f977b564b6169621be7bf23c8d43e7ea
dathalong710 <dathalongbay@gmail.com>
2019-06-11 15:08:53 +0700

    Add files via upload

---
+++ mvc/models/ProductModel.php
@@ -0,0 +1,29 @@
+<?php
+namespace MVC\Models;
+
+class ProductModel extends Database {
+
+
+    public function getAll() {
+
+    }
+
+
+    public function getSingle() {
+
+    }
+
+
+    public function store() {
+
+    }
+
+    public function update() {
+
+    }
+
+
+    public function delete() {
+
+    }
+}
